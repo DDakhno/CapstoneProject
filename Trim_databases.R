c16166f619ee80030c61e8d0ebaaff2a1987d5f8
DDakhno <d.dakhno@gmail.com>
2016-09-27 10:58:02 +0200

    Reload DBs works incl. update of the table

--- Trim_databases.R
+++ Trim_databases.R
@@ -1,17 +1,56 @@
-load("DB/dt_trigrams/dt_trigrams.Rdata")
+library(data.table)
+library(dplyr)
 
-head(db_joined)
+START <- as.integer(Sys.time())
+dbs <- c("DB/dt_monograms/dt_monograms.Rdata" , "DB/dt_bigrams/dt_bigrams.Rdata" , "DB/dt_trigrams/dt_trigrams.Rdata", "DB/dt_four_grams/dt_four_grams.Rdata", "DB/dt_pentagrams/dt_pentagrams.Rdata")
 
-dim(db_joined)
+max_rank <- 150
 
-length(unique(db_joined$predictor))
-
-prds <- unique(db_joined$predictor)
-
-db_max_top100_each_pred <- data.table()
-tmp <- data.table()
-
-for (prd in prds[1:1000]) {
-    tmp <- na.omit(db_joined[prd]%>%arrange(desc(freq))%>%slice(1:10))
-    db_max_top100_each_pred <- bind_rows(db_max_top100_each_pred,tmp)
+myrank <- function(x) {
+    
+    if(x == lastx) {
+        cnt <<- cnt + 1
+    } else {
+        cnt <<- 1
+    }
+    lastx <<- x
+    cnt
 }
+
+for (db in dbs) {
+    
+    db <- "DB/dt_trigrams/dt_trigrams.Rdata"
+    
+    print(db)
+    
+    load(db)
+    
+    if (grepl("monogram",db)) {
+        
+        # Cleaning from the apparently non-regular words and expressions (mainly from Twitter)
+        db_joined <- as.data.table(db_joined %>% filter(! predictor %like% "^(##.*|[=<:\x5b\x5e\x7e\x7c\x7b_`]{2,})|^[^a-zA-Z]+$|([:;])$" ))
+        
+        
+    } else {
+        
+        # Cleaning from the apparently non-regular words and expressions (mainly from Twitter)
+        # Removing "words" like "##$&' or "joined;", leaving regular words and hashtags
+        
+        db_joined <- as.data.table(db_joined %>% slice %>% filter(predictor %like% "^[a-z#][a-z]" & ! predictor %like% " [^a-z#]" & ! predictor %like% "[;:!.]$|[;:!.] "))
+        
+        
+        lastx <- ""
+        cnt <- 0
+       
+        print(system.time(ranks <- sapply(db_joined$predictor, myrank)))
+        
+        mask <- ranks <= max_rank
+        db_joined$rank <- ranks
+        db_joined <- as.data.table(db_joined[mask])
+    }
+    
+    setkey(db_joined,predictor)
+    
+    save(db_joined, file=gsub("[.]",paste("_max_rank_",max_rank,".",sep = ""),db))
+}
+
